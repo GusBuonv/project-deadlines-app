010056036afd5e4a7e57feaf74d2fb30ed019614
Augustus Buonviri <abuonviri@gmail.com>
2021-07-08 21:45:52 -0500

    Integrate styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ReactElement } from 'react';
+import { ThemeProvider } from 'styled-components';
+import * as theme from '../public/default-theme.json';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return (
+    <ThemeProvider theme={theme}>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-export default MyApp
