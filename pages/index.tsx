7bb2c3b5453fcbfa0057129f1d9fc44e31277e09
Augustus Buonviri <abuonviri@gmail.com>
2021-07-08 21:47:57 -0500

    Clean up after create next app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { ReactElement } from 'react';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home(): ReactElement {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,11 +13,14 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to
+          {' '}
+          <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing
+          {' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -53,17 +56,12 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Made for
+        {' '}
+        <span className={styles.logo}>
+          <img src="/koala-logo.png" alt="Koala Logo" width="72" height="16" />
+        </span>
       </footer>
     </div>
-  )
+  );
 }
