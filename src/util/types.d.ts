519e84619a9e34b61f8512390ea9670b6abb4f73
Augustus Buonviri <abuonviri@gmail.com>
2021-07-11 20:48:12 -0500

    Format type declaration

--- src/util/types.d.ts
+++ src/util/types.d.ts
@@ -1,8 +1,10 @@
 import { Dispatch, SetStateAction } from 'react';
 
-type WithClassName<P extends Record<string, unknown>> = P
+type WithClassName<P extends Record<string, unknown>> = (
+  & P
   & {
     className?: string,
-  };
+  }
+);
 
 type SetDuration = Dispatch<SetStateAction<Duration | null>>;
