fa8534b0d8b0bdc6abb07d253c0a0de31ae09525
Augustus Buonviri <abuonviri@gmail.com>
2021-07-15 07:18:24 -0500

    Add lang attribute

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -185,6 +185,7 @@ const HomeRaw = ({
         <meta name="description" content={description} />
         <meta name="robots" content="noindex" />
         <link rel="icon" href={favicon} />
+        <html lang="en" />
       </Head>
 
       <StyledMain $tall={projectListIds.length !== 0}>
