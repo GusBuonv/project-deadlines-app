3927c22e33866c92cdc245c04d38bd3c04720c99
Augustus Buonviri <abuonviri@gmail.com>
2021-07-22 20:15:04 -0500

    Update icon

--- src/components/ui/icon.tsx
+++ src/components/ui/icon.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
 import styled from 'styled-components';
-import { WithClassName } from '../../util/types';
+import { HasClassName } from '../../util/types';
 
 //
 // Styles
@@ -14,7 +14,7 @@ const StyledSVG = styled.svg<{ $fill?: string }>`
 // Component
 //
 
-const iconPaths = {
+const paths = {
   save: 'M15,9H5V5H15M12,19A3,3 0 0,1 9,16A3,3 0 0,1 12,13A3,3 0 0,1 15,16A3,3 0 0,1 12,19M17,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V7L17,3Z',
   delete: 'M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z',
   pencil: 'M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z',
@@ -24,30 +24,30 @@ const iconPaths = {
   close: 'M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z',
 } as const;
 
-export type IconType = keyof typeof iconPaths;
-export type IconSize = 'tiny' | 'small' | 'medium' | 'large';
-
-const iconSizes: { readonly [K in IconSize]: number } = {
+const sizes = {
   tiny: 30,
   small: 40,
   medium: 64,
   large: 80,
-};
+} as const;
 
-export interface IconProps {
+export type IconType = keyof typeof paths;
+export type IconSize = keyof typeof sizes;
+
+export interface IconProps extends HasClassName {
   size: IconSize,
   icon: IconType,
   color?: string,
 }
 
-const IconRaw = ({
+const Icon = ({
   className,
   size,
   icon,
   color,
-}: WithClassName<IconProps>): JSX.Element => {
-  const px = iconSizes[size];
-  const path = iconPaths[icon];
+}: IconProps): JSX.Element => {
+  const px = sizes[size];
+  const path = paths[icon];
 
   return (
     <StyledSVG
@@ -62,6 +62,5 @@ const IconRaw = ({
   );
 };
 
-const Icon = styled(memo(IconRaw))``;
-
-export default Icon;
+/** An SVG icon */
+export default styled(memo(Icon))``;
