d1f63ab26038a631af7a42a49583fe8e66847f18
Augustus Buonviri <abuonviri@gmail.com>
2021-07-22 19:43:25 -0500

    Update sr-only-label

--- src/components/ui/sr-only-label.tsx
+++ src/components/ui/sr-only-label.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { SrOnlyCSS } from '../../styles';
 
+/** \<label\> element that is visible only to screen readers */
 const SrOnlyLabel = styled.label`
   ${SrOnlyCSS}
 `;
